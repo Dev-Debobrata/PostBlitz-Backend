7736cf4c0f24bb9e4181097d0b06b816bb2bd55d
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-10-23 23:36:12 +0530

    type for user

---
+++ src/utils/typings.ts
@@ -0,0 +1,10 @@
+export type IUser = {
+    id: String,
+    name: String,
+    username: String,
+    password: String,
+    email: String,
+    address: String,
+    pincode: Number,
+    country: String,
+}

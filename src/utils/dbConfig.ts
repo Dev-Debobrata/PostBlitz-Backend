100b795656214feef954d1631a858e6769e80ec6
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-10-23 23:35:48 +0530

    added mongoURL

--- src/utils/dbConfig.ts
+++ src/utils/dbConfig.ts
@@ -1,12 +1,10 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from "dotenv";
 import { connect } from "mongoose";
 
-dotenv.config({ path: __dirname+'/../.env' })
+dotenv.config({ path: __dirname + "/../.env" });
 
-const mongoURL: String | undefined = process.env.MONGO_URL
-
-console.log(mongoURL)
+const mongoURL: String | undefined = process.env.MONGO_URL;
 
 export const connectToDatabase = async () => {
-    await connect(`${mongoURL}`)
-}
+  await connect(`${mongoURL}`);
+};

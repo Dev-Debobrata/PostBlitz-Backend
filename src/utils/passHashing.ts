6623b6d69d2e529b6695c85a742ac49f00e737ba
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-10-29 23:44:00 +0530

    changed promise type

--- src/utils/passHashing.ts
+++ src/utils/passHashing.ts
@@ -1,6 +1,6 @@
 import { compare, genSalt, hash } from 'bcrypt';
 
-export const passHash = async (password: string): Promise<string> => {
+export const passHash = async (password: string): Promise<any> => {
     const salt = await genSalt(10);
     const hashed = await hash(password, salt);
     return hashed;

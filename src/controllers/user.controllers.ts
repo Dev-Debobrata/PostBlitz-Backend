c3768fa9b82acfed6bf7265b214cddce48072ac6
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-11-06 23:26:18 +0530

    updated postUser and Get User function

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,19 +1,46 @@
 import { Request, response, Response } from "express";
 import { HydratedDocument } from "mongoose";
 import { User } from "../models/user.model";
-import { Blog } from "../models/blog.model";
-import { IBlog, IUser } from "../utils/typings";
+import { IUser } from "../utils/typings";
 import { passHash } from "../utils/passHashing";
 import { generateToken } from "../utils/token";
 import { upload } from "../utils/multerConfig";
 
-export const getUser = async (req: Request, res: Response): Promise<any> => {
+export const getUsers = async (req: Request, res: Response): Promise<any> => {
   try {
-    const { username } = req.body;
+    const users: Array<IUser> | null = await User.find(
+      {},
+      "name username country"
+    ).populate("blogs", "-author");
+    if (users === null) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "Success!" });
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-    // const user = await User.findOne<IUser>({ username });
-    const result = await User.findOne<IUser>({ username }).populate("blogs", "-_id-userID")
-    res.status(200).json(result);
+export const getUserByUsername = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const { username } = req.params;
+
+    const user: IUser | null = await User.findOne(
+      { username },
+      "name username country"
+    ).populate("blogs", "-author-password");
+    if (user === null) {
+      res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({
+      name: user?.name,
+      username: user?.username,
+      country: user?.country,
+      blogs: user?.blogs,
+    });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
@@ -24,8 +51,8 @@ export const postUser = async (req: Request, res: Response): Promise<any> => {
     let { name, username, password, email, address, pincode, country } =
       req.body;
 
-    const foundUser = await User.findOne<IUser>({ username });
-    if (foundUser) {
+    const foundUser: IUser | null = await User.findOne({ username });
+    if (foundUser !== null) {
       return res.status(400).json({
         message: "Username already exists",
       });

8d8bca8f1fb3492a2c3947d19b60e135bb4f4adc
Debobrata Pal <dev.debobratapal@gmail.com>
2022-11-07 22:44:37 +0530

    Delete passHashing.ts

--- src/utils/passHashing.ts
+++
@@ -1,7 +0,0 @@
-import { genSalt, hash } from 'bcrypt';
-
-export const passHash = async (password: string): Promise<string> => {
-    const salt = await genSalt(10);
-    const hashed = await hash(password, salt);
-    return hashed.toString();
-}

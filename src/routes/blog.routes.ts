e353a3e7ecae09e1bfdd14128b28648367858b86
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-11-09 23:38:09 +0530

    added routes for fetching individual blog

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getBlogs } from "../controllers/blogs/getBlogs.controllers";
+import { getBlogById, getBlogByTitle, getBlogs } from "../controllers/blogs/getBlogs.controllers";
 import { postBlog } from "../controllers/blogs/postBlogs.controllers";
 
 export const blogRouter = Router()
 
 blogRouter.get("/blogs", getBlogs);
-// blogRouter.get("/blogs/:title", getBlog);
+blogRouter.get("/blog/id/:_id", getBlogById);
+blogRouter.get("/blog/title/:title", getBlogByTitle);
 
 blogRouter.post("/blogs", postBlog);

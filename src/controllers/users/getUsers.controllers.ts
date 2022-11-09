966b7916712ae040337c6042c26858c6fab4b4f2
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-11-09 23:37:07 +0530

    added fetch user by id and title

--- src/controllers/users/getUsers.controllers.ts
+++ src/controllers/users/getUsers.controllers.ts
@@ -17,22 +17,42 @@ export const getUsers = async (req: Request, res: Response): Promise<any> => {
   }
 };
 
-// export const getUserByUsername = async (
-//   req: Request,
-//   res: Response
-// ): Promise<any> => {
-//   try {
-//     const { username } = req.params;
+export const getUserByID = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const { _id } = req.params;
+    const user: IUser | null = await User.findById(
+      {_id: _id},
+      "name username country"
+    ).populate("blogs", "-author");
+    if (user === null) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-//     const user: IUser | null = await User.findOne(
-//       { username },
-//       "name username country"
-//     ).populate("blogs", "-author-password");
-//     if (user === null) {
-//       res.status(404).json({ message: "User not found" });
-//     }
-//     res.status(200).json(user);
-//   } catch (error: any) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
+export const getUserByUsername = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const { username } = req.params;
+    const user: IUser | null = await User.findOne(
+      { username: username },
+      "name username country"
+    ).populate("blogs", "-author");
+    if (user === null) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};

04626ac9cf809a5d25a00f57c79f6f63c03b4d50
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-11-10 23:00:48 +0530

    added fetch users by category function

--- src/controllers/blogs/getBlogs.controllers.ts
+++ src/controllers/blogs/getBlogs.controllers.ts
@@ -9,10 +9,9 @@ export const getBlogs = async (req: Request, res: Response): Promise<any> => {
       "-_id name username"
     );
     if (blogs === null) {
-      res.status(404).json({ message: "Blog Not Found" });
-    } else {
-      res.status(200).json(blogs);
+      return res.status(404).json({ message: "Blog Not Found" });
     }
+    res.status(200).json(blogs);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
@@ -29,10 +28,9 @@ export const getBlogById = async (
       "name username"
     );
     if (blog === null) {
-      res.status(404).json({ message: "Blog not found" });
-    } else {
-      res.status(200).json(blog);
+      return res.status(404).json({ message: "Blog not found" });
     }
+    res.status(200).json(blog);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
@@ -44,16 +42,34 @@ export const getBlogByTitle = async (
 ): Promise<any> => {
   try {
     const { title } = req.params;
-    const blog: Array<IBlog> | null = await Blog.find({ title }).populate(
-      "author",
-      "name username"
-    );
+    const blog: Array<IBlog> | null = await Blog.find({
+      title: { $regex: new RegExp(title.replace(/\s+/g, "\\s+"), "gi") },
+    }).populate("author", "name username");
     if (blog === null) {
-      res.status(404).json({ message: "Blog not found" });
-    } else {
-      res.status(200).json(blog);
+      return res.status(404).json({ message: "Blog not found" });
     }
+    res.status(200).json(blog);
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
-}; // Work in Progress
+};
+
+export const getBlogByCategory = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    const { category } = req.params;
+    const blog: Array<IBlog> | null = await Blog.find({
+      categories: {
+        $regex: new RegExp(category.replace(/\s+/g, "\\s+"), "gi"),
+      },
+    }).populate("author", "name username");
+    if (blog === null) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    res.status(200).json(blog);
+  } catch (error: any) {
+    res.status(500).json({ message: error.message });
+  }
+};

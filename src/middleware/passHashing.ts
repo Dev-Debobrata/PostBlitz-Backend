d5ea2f4194343a69b6d83594544d677753c24965
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-11-10 23:01:32 +0530

    added password verification

--- src/middleware/passHashing.ts
+++ src/middleware/passHashing.ts
@@ -1,7 +1,14 @@
-import { genSalt, hash } from 'bcrypt';
+import { genSalt, hash, compare } from "bcrypt";
 
 export const passHash = async (password: string): Promise<string> => {
-    const salt = await genSalt(10);
-    const hashed = await hash(password, salt);
-    return hashed.toString();
-}
+  const salt = await genSalt(10);
+  const hashed = await hash(password, salt);
+  return hashed.toString();
+};
+
+export const IsValidUser = async (
+  password: string,
+  userPassword: string
+): Promise<boolean> => {
+  return await compare(password, userPassword);
+};

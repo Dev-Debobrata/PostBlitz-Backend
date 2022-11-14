e93f27c6ead53e3cccfef5236b9270148dfc5751
dev-debobrata <dev.debobratapal@gmail.com>
2022-11-14 23:59:44 +0530

    updated type definition

--- src/utils/typings.ts
+++ src/utils/typings.ts
@@ -1,26 +1,28 @@
 export type IUser = {
-    id: String,
-    name: String,
-    username: String,
-    password: String,
-    image: String,
-    email: String,
-    address: String,
-    pincode: Number,
-    country: String,
-    blogs: String[]
-}
+  id: String;
+  name: String;
+  username: String;
+  password: String;
+  image: String;
+  email: String;
+  address: String;
+  pincode: Number;
+  country: String;
+  blogs: {
+    blogID: String;
+    liked: Boolean;
+  };
+};
 
 export type IBlog = {
-    id: String,
-    author: String,
-    title: String,
-    description: String,
-    content: String,
-    categories: String[],
-    images: String[],
-    urls: String[],
-    likes: Number,
-    shareLink: string,
-}
-
+  id: String;
+  author: String;
+  title: String;
+  description: String;
+  content: String;
+  categories: String[];
+  images: String[];
+  urls: String[];
+  likes: String[];
+  shareLink: string;
+};

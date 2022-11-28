5afa451ecc940f8285c256b1916ebd73f0a52deb
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-11-28 23:35:47 +0530

    middleware function to upload files in s3

---
+++ src/middleware/fileHandlers.ts
@@ -0,0 +1,31 @@
+import { createReadStream } from "fs";
+import { s3 } from "../utils/s3Config";
+import * as dotenv from "dotenv";
+dotenv.config({ path: __dirname + "/../.env" });
+
+export const fileUpload = (file: Express.Multer.File) => {
+  const fileStream = createReadStream(file.path);
+
+  const uploadParams = {
+    Bucket: `${process.env.AWS_S3_BUCKET_NAME}`,
+    Body: fileStream,
+    Key: file.filename,
+  };
+
+  return s3.upload(uploadParams).promise();
+};
+
+export const filesUpload = async (files: Array<Express.Multer.File>) => {
+  const params = files.map((file) => {
+    return {
+      Bucket: `${process.env.AWS_S3_BUCKET_NAME}`,
+      Body: file.buffer,
+      Key: file.filename,
+    };
+  });
+
+  const results = await Promise.all(
+    params.map((param) => s3.upload(param).promise())
+  );
+  return results;
+};

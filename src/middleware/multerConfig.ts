bc41e496c4bdf395401ca8def5fa67cd28f80a51
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-11-28 23:35:20 +0530

    added file filter and limits of storage

--- src/middleware/multerConfig.ts
+++ src/middleware/multerConfig.ts
@@ -1,20 +1,38 @@
 import { Request } from "express";
-import multer, { FileFilterCallback } from "multer";
+import multer, { diskStorage } from "multer";
+import { v4 } from "uuid";
 
 type DestinationCallback = (error: Error | null, destination: string) => void;
 type FileNameCallback = (error: Error | null, filename: string) => void;
 
-const dpStorage = multer.diskStorage({
+const dpStorage = diskStorage({
   destination: (
     req: Request,
     file: Express.Multer.File,
     cb: DestinationCallback
   ) => {
-    cb(null, "uploads");
+    cb(null, "./src/uploads/");
   },
   filename: (req: Request, file: Express.Multer.File, cb: FileNameCallback) => {
-    cb(null, file.originalname);
+    const { originalname } = file;
+    cb(null, `${v4()}-${originalname}`);
   },
 });
 
-export var upload = multer({ storage: dpStorage });
+const fileFilter = (req: Request, file: Express.Multer.File, cb: any) => {
+  if (
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png"
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Image uploaded is not of type jpg/jpeg or png"), false);
+  }
+};
+
+export var upload = multer({
+  storage: dpStorage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 2000000, files: 3 },
+});

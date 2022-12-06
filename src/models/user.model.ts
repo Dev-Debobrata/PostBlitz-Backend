9d463d508876b0c68b60bb70e73fc9f72b79d17f
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-12-07 00:49:54 +0530

    added mongoose validation

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,35 +8,50 @@ const userSchema = new Schema<IUser>({
   },
   name: {
     type: String,
-    required: true,
+    required: [true, "Name is required"],
+    maxlength: [100, "Name is too long"],
   },
   username: {
     type: String,
-    required: true,
+    required: [true, "Username is required"],
+    minlength: [6, "Minimum 6 characters are required"],
+    maxlength: [30, "Username should not be more than 30 characters"],
   },
   password: {
     type: String,
-    required: true,
+    required: [true, "Password is required"],
+    match: [
+      /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+      "Password must be at least 8 characters with one Uppercase, one lowercase, one special character and one number",
+    ],
   },
   image: {
-    data: Buffer,
-    contentType: String,
+    type: String,
+    required: false,
   },
   email: {
     type: String,
-    required: true,
+    required: [true, "Email is required"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Not a valid email",
+    ],
   },
   address: {
     type: String,
-    required: true,
+    required: [true, "Address is required"],
+    maxlength: [150, "Address is too long"],
   },
   pincode: {
     type: String,
-    required: true,
+    required: [true, "Pincode is required"],
+    minlength: [6, "Pincode is invalid"],
+    maxlength: [6, "Pincode is invalid"],
   },
   country: {
     type: String,
-    required: true,
+    required: [true, "Country is required"],
+    maxlength: [50, "Country is too long"],
   },
   blogs: [
     {

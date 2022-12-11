e57ad9c012ef52f989d251ed4d688920d363ec6d
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-12-11 19:16:04 +0530

    fixed redis configuration

--- src/utils/redisConfig.ts
+++ src/utils/redisConfig.ts
@@ -1,8 +1,9 @@
 import * as dotenv from "dotenv";
-import { createClient, RedisClientOptions } from "redis";
+import { createClient } from "redis";
+import { logger } from './logger';
 
 dotenv.config({ path: __dirname + "/../.env" });
 
 export const redisClient = createClient({
   url: `${process.env.REDIS_URL}`,
-});
+}); 

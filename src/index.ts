4da01fe9370c50ca190478bdd05314455dc1a8e6
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-12-11 19:14:08 +0530

    fixed syntax

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 import { app } from './app';
 
-dotenv.config({ path: __dirname+'/.env' })
+dotenv.config({ path: __dirname + '/.env' });
 
-const host: String | undefined = process.env.HOST
-const port: String | undefined = process.env.PORT
+const host: String | undefined = process.env.HOST;
+const port: String | undefined = process.env.PORT;
 
 app.listen(port, () => {
-    console.log(`Server is running at http://${host}:${port}`)
-})
+  console.log(`Server is running at http://${host}:${port}`);
+});

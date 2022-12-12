04fe22aa355e48c1e306ec315548a10036044322
Dev-Debobrata <dev.debobratapal@gmail.com>
2022-12-12 16:05:12 +0530

    removed logger

--- src/utils/redisConfig.ts
+++ src/utils/redisConfig.ts
@@ -1,12 +1,10 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 import { createClient } from "redis";
-import { logger } from './logger';
-
-dotenv.config({ path: __dirname + "/../.env" });
+dotenv.config({ path: __dirname + '/../../.env' });
 
 /**
  * @description - Connects to Redis Cluster
- */
+*/
 
 export const redisClient = createClient({
   url: `${process.env.REDIS_URL}`,
